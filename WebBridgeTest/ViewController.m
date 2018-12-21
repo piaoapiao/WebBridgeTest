0986198aad2acb1eab91219dd4bb71f279a32be5
guodong <guodong@guodongdeMacBook-Pro.local>
2018-12-21 10:51:26 +0800

    success load  web content

--- WebBridgeTest/ViewController.m
+++ WebBridgeTest/ViewController.m
@@ -7,17 +7,59 @@
 //
 
 #import "ViewController.h"
+#import "WebViewJavascriptBridge.h"
 
 @interface ViewController ()
-
+@property (nonatomic,strong)  UIWebView *webView;
+@property (nonatomic,strong)  NSString *urlString;
+@property WebViewJavascriptBridge *bridge;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.urlString = @"http://oneccc.bid/WebBridgeTest/bridge.html";
+//    self.urlString = @"https://baidu.com";
+    [self createBridge];
+    [self registerMessage];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(UIWebView *)webView
+{
+    if(!_webView)
+    {
+        _webView = [[UIWebView alloc] initWithFrame:self.view.frame];
+//        _webView.backgroundColor = [UIColor greenColor];
+        [self.view addSubview:_webView];
+    }
+    return _webView;
+}
+
+
+-(void)createBridge
+{
+    NSString *encodedString = [self.urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    NSURL *url = [NSURL URLWithString:encodedString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    self.webView.delegate = self;
+    [self.webView loadRequest:request];
+    //   jsBridge 处理与js的交互
+    [WebViewJavascriptBridge enableLogging];
+    _bridge = [WebViewJavascriptBridge bridge:_webView];
+    
+}
+
+-(void)registerMessage
+{
+    [self.bridge registerHandler:@"showAppPage" handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSDictionary *dataDict = (NSDictionary *)data;
+        NSLog(@"dataDict:%@",data);
+    }];
+    
+}
 
 @end
+
+     
