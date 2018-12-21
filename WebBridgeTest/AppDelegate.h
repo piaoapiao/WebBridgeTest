4404345d63152d64af23023a53b84aadfc5075af
guodong <>
2018-12-21 10:23:47 +0800

    create project

---
+++ WebBridgeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebBridgeTest
+//
+//  Created by guodong on 2018/12/21.
+//  Copyright © 2018年 Maizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebBridgeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebBridgeTest
+//
+//  Created by guodong on 2018/12/21.
+//  Copyright © 2018年 Maizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
